dea6c83bfd98597641554d181ded9d761bbca40f
Leon <ChinL@outlook.co.nz>
2020-10-20 15:50:40 +1300

    Created detailed tests for TestAddStudent.
    Needed to add new lines to end of original data files to enable execution

--- src/test/java/TestAddStudent.java
+++ src/test/java/TestAddStudent.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,6 +17,9 @@ public class TestAddStudent extends AbstractTestProcess{
         refactoredProcess = new RefactoredProcess();
     }
 
+    /**
+     * Tests general case where user creates a student
+     */
     @Test
     public void testAddStudent() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
         List<String> inputList = new ArrayList<>();
@@ -31,4 +35,70 @@ public class TestAddStudent extends AbstractTestProcess{
         compareOutputsBetweenRefactoredAndOriginal(inputList);
     }
 
+    /**
+     * Tests case where user creates student while asking for help where possible
+     */
+    @Test
+    public void testAddStudentWithHelp() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("1"); // Add a student
+        inputList.add("1"); // Manually enter student ID
+        inputList.add("U1829394D"); // Enter student ID
+        inputList.add("Gerald"); // Enter student name
+        inputList.add("-h"); // List all departments
+        inputList.add("ECSE"); // Enter student department
+        inputList.add("-h"); // List all genders
+        inputList.add("MALE"); // Enter student gender
+        inputList.add("1"); // Enter student school year
+        inputList.add("11"); // Exit program
+
+        compareOutputsBetweenRefactoredAndOriginal(inputList);
+    }
+
+    /**
+     * Tests case where user creates student while asking for help where possible
+     */
+    @Test
+    public void testAddStudentErrors() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("1"); // Add a student
+        inputList.add("1"); // Manually enter student ID
+        inputList.add("INVALIDID"); // Enter invalid student ID
+        inputList.add("U1829394E"); // Enter student ID
+        inputList.add("Gerald"); // Enter student name
+        inputList.add("INVALIDDEPARTMENT"); // Enter invalid department
+        inputList.add("ECSE"); // Enter student department
+        inputList.add("INVALIDGENDER"); // Enter invalid gender
+        inputList.add("MALE"); // Enter student gender
+        inputList.add("-1"); // Enter OOB student school year
+        inputList.add("5"); // Enter OOB student school year
+        inputList.add("1"); // Enter student school year
+        inputList.add("11"); // Exit program
+
+        compareOutputsBetweenRefactoredAndOriginal(inputList);
+    }
+
+    /**
+     * Tests case where user creates student while generating student automatically
+     */
+    @Test
+    public void testAddStudentAutomatically() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("1"); // Add a student
+        inputList.add("2"); // Auto generate student ID
+        inputList.add("Geraldine"); // Enter student name
+        inputList.add("CS"); // Enter student department
+        inputList.add("MALE"); // Enter student gender
+        inputList.add("-1"); // Enter OOB student school year
+        inputList.add("5"); // Enter OOB student school year
+        inputList.add("1"); // Enter student school year
+        inputList.add("11"); // Exit program
+
+        // Assert that the student is created and printed
+        Assert.assertTrue(refactoredProcess.getOutput(inputList).contains("| Geraldine | CS | MALE | 1 | not available"));
+
+        // Need to replace both data files as the data could be different
+        originalProcess = new OriginalProcess();
+        refactoredProcess = new RefactoredProcess();
+    }
 }
