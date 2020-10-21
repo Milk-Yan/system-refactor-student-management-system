9c295d49380b465b5730fb8aa0ef7c68d4c3dfac
Leon <ChinL@outlook.co.nz>
2020-10-21 14:46:05 +1300

    Created detailed tests forTestCheckAvailableSlotsInClass

--- src/test/java/TestCheckAvailableSlotsInClass.java
+++ src/test/java/TestCheckAvailableSlotsInClass.java
@@ -25,4 +25,27 @@ public class TestCheckAvailableSlotsInClass extends AbstractTestProcess{
         compareOutputsBetweenRefactoredAndOriginal(inputList);
     }
 
+    @Test
+    public void testCheckAvailableSlotsInClassErrors() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("4"); // Check available slots in class
+        inputList.add("INVALIDCOURSEID"); // Enter invalid course ID
+        inputList.add("SE2001"); // Enter course ID
+        inputList.add("11"); // Exit program
+
+        compareOutputsBetweenRefactoredAndOriginalDebug(inputList);
+    }
+
+
+    @Test
+    public void testCheckAvailableSlotsInClassWithHelp() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("4"); // Check available slots in class
+        inputList.add("-h"); // List all course IDs
+        inputList.add("SE2001"); // Enter course ID
+        inputList.add("11"); // Exit program
+
+        compareOutputsBetweenRefactoredAndOriginalDebug(inputList);
+    }
+
 }
