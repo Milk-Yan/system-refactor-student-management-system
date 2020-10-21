f54d58d1a6be841a2907cd48fae5f237ca177d25
Leon <ChinL@outlook.co.nz>
2020-10-21 19:34:20 +1300

    Created detailed tests for TestPrintStudentTranscript

--- src/test/java/TestPrintStudentTranscript.java
+++ src/test/java/TestPrintStudentTranscript.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-public class TestPrintStudentTranscript extends AbstractTestProcess{
+public class TestPrintStudentTranscript extends AbstractTestProcess {
 
     @BeforeClass
     public static void setupResources() throws URISyntaxException, IOException {
@@ -15,6 +15,9 @@ public class TestPrintStudentTranscript extends AbstractTestProcess{
         refactoredProcess = new RefactoredProcess();
     }
 
+    /**
+     * Tests general case where user gets student transcript
+     */
     @Test
     public void testPrintStudentTranscript() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
         List<String> inputList = new ArrayList<>();
@@ -22,7 +25,38 @@ public class TestPrintStudentTranscript extends AbstractTestProcess{
         inputList.add("U1234567L"); // Enter course ID
         inputList.add("11"); // Exit program
 
-        // Compare lines used as original system uses HashMap#entrySet() which is pseudorandom
+        // Compare output lines as original system uses HashMap#entrySet() which is pseudorandom
         compareLinesBetweenRefactoredAndOriginal(inputList);
     }
+
+    /**
+     * Tests case where user gets student transcript with help
+     */
+    @Test
+    public void testPrintStudentTranscriptWithHelp() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("10"); // Print course statistics
+        inputList.add("-h"); // List all course statistics
+        inputList.add("U1234567L"); // Enter course ID
+        inputList.add("11"); // Exit program
+
+        // Compare output lines as original system uses HashMap#entrySet() which is pseudorandom
+        compareOutputsBetweenRefactoredAndOriginal(inputList);
+    }
+
+    /**
+     * Tests case where user gets student transcript encountering every error statements
+     */
+    @Test
+    public void testPrintStudentTranscriptErrors() throws IOException, InterruptedException, URISyntaxException, TimeoutException {
+        List<String> inputList = new ArrayList<>();
+        inputList.add("10"); // Print course statistics
+        inputList.add("INVALIDSTUDENT"); // Enter invalid course
+        inputList.add("U1234567L"); // Enter course ID
+        inputList.add("11"); // Exit program
+
+        // Compare output lines as original system uses HashMap#entrySet() which is pseudorandom
+        compareOutputsBetweenRefactoredAndOriginal(inputList);
+    }
+
 }
