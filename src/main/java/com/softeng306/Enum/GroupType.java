e39df1b763e85cfa1d4eb258fb5cee93287baa83
Milk-Yan <eyan868@aucklanduni.ac.nz>
2020-10-25 23:37:18 +1300

    Adjusted enums on original data.

--- src/main/java/com/softeng306/Enum/GroupType.java
+++ src/main/java/com/softeng306/Enum/GroupType.java
@@ -1,7 +1,7 @@
 package com.softeng306.Enum;
 
 public enum GroupType {
-    LAB_GROUP("LabGroup"), LECTURE_GROUP("LectureGroup"), TUTORIAL_GROUP("TutorialGroup");
+    LAB_GROUP("lab"), LECTURE_GROUP("lecture"), TUTORIAL_GROUP("tutorial");
 
     private String stringValue;
 
