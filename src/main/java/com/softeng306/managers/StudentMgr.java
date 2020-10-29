8a5c77a4266fab2da088bcb4fad5e507b48e25ac
ArtificialSnow <plin213@aucklanduni.ac.nz>
2020-10-29 18:05:30 +1300

    refactor getStudentFromId

--- src/main/java/com/softeng306/managers/StudentMgr.java
+++ src/main/java/com/softeng306/managers/StudentMgr.java
@@ -3,6 +3,7 @@ package com.softeng306.managers;
 
 import com.softeng306.domain.course.component.MainComponent;
 import com.softeng306.domain.course.component.SubComponent;
+import com.softeng306.domain.exceptions.StudentNotFoundException;
 import com.softeng306.domain.mark.MainComponentMark;
 import com.softeng306.domain.mark.Mark;
 import com.softeng306.domain.mark.MarkCalculator;
@@ -208,13 +209,17 @@ public class StudentMgr {
         return !studentCourses.isEmpty();
     }
 
-    public Student getStudentFromId(String studentId) {
-        Optional<Student> optionalStudent = students.stream().filter(s -> studentId.equals(s.getStudentID())).findFirst();
-        if (optionalStudent.isPresent()) {
-            return optionalStudent.get();
-        } else {
-            return null;
+    public Student getStudentFromId(String studentId) throws StudentNotFoundException {
+        Optional<Student> student = students
+                .stream()
+                .filter(s -> studentId.equals(s.getStudentID()))
+                .findFirst();
+
+        if (!student.isPresent()) {
+            throw new StudentNotFoundException(studentId);
         }
+
+        return student.get();
     }
 
     public String getStudentName(String studentId) {
