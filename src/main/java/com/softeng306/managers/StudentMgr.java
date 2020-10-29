171569fe8d66e32a6d697e112591f13d565b9967
shelrod24 <srdo536@aucklanduni.ac.nz>
2020-10-30 03:22:00 +1300

    Update StudentMgr.java
    remove unused imports and huge unused printStudentTranscript method
    
    Co-Authored-By: ArtificialSnow <12625705+ArtificialSnow@users.noreply.github.com>

--- src/main/java/com/softeng306/managers/StudentMgr.java
+++ src/main/java/com/softeng306/managers/StudentMgr.java
@@ -1,12 +1,7 @@
 package com.softeng306.managers;
 
-import com.softeng306.domain.course.component.MainComponent;
-import com.softeng306.domain.course.component.SubComponent;
 import com.softeng306.domain.exceptions.StudentNotFoundException;
-import com.softeng306.domain.mark.MainComponentMark;
-import com.softeng306.domain.mark.Mark;
 import com.softeng306.domain.mark.MarkCalculator;
-import com.softeng306.domain.mark.SubComponentMark;
 import com.softeng306.domain.student.Student;
 
 import com.softeng306.fileprocessing.IFileProcessor;
@@ -14,8 +9,6 @@ import com.softeng306.fileprocessing.StudentFileProcessor;
 
 import com.softeng306.enums.Department;
 import com.softeng306.enums.Gender;
-
-import com.softeng306.io.StudentMgrIO;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,78 +62,6 @@ public class StudentMgr {
 
         studentFileProcessor.writeNewEntryToFile(currentStudent);
         students.add(currentStudent);
-    }
-
-    /**
-     * Prints transcript (Results of course taken) for a particular student
-     */
-    public void printStudentTranscript() {
-        String studentID = new StudentMgrIO().readStudentIdFromUser();
-
-        double studentGPA = 0d;
-        int thisStudentAU = 0;
-
-        List<Mark> thisStudentMark = new ArrayList<>();
-        for (Mark mark : MarkMgr.getInstance().getMarks()) {
-            if (mark.getStudent().getStudentID().equals(studentID)) {
-                thisStudentMark.add(mark);
-                thisStudentAU += mark.getCourse().getAU();
-            }
-        }
-
-        if (thisStudentMark.isEmpty()) {
-            System.out.println("------ No transcript ready for this student yet ------");
-            return;
-        }
-        System.out.println("----------------- Official Transcript ------------------");
-        System.out.print("Student Name: " + thisStudentMark.get(0).getStudent().getStudentName());
-        System.out.println("\tStudent ID: " + thisStudentMark.get(0).getStudent().getStudentID());
-        System.out.println("AU for this semester: " + thisStudentAU);
-        System.out.println();
-
-
-        for (Mark mark : thisStudentMark) {
-            System.out.print("Course ID: " + mark.getCourse().getCourseID());
-            System.out.println("\tCourse Name: " + mark.getCourse().getCourseName());
-
-            for (MainComponentMark mainComponentMark : mark.getCourseWorkMarks()) {
-                MainComponent mainComponent = mainComponentMark.getMainComponent();
-                Double result = mainComponentMark.getMark();
-
-                System.out.println("Main Assessment: " + mainComponent.getComponentName() + " ----- (" + mainComponent.getComponentWeight() + "%)");
-
-                for (SubComponentMark subComponentMark : mainComponentMark.getSubComponentMarks()) {
-                    SubComponent subComponent = subComponentMark.getSubComponent();
-                    System.out.print("Sub Assessment: " + subComponent.getComponentName() + " -- (" + subComponent.getComponentWeight() + "% * " +
-                            mainComponent.getComponentWeight() + "%) --- ");
-
-                    String subAssessmentName = subComponent.getComponentName();
-                    System.out.println("Mark: " + String.valueOf(subComponentMark.getMark()));
-                }
-
-                System.out.println("Main Assessment Total: " + result);
-                System.out.println();
-            }
-
-            System.out.println("Course Total: " + mark.getTotalMark());
-            studentGPA += markCalculator.gpaCalculator(mark) * mark.getCourse().getAU();
-            System.out.println();
-        }
-        studentGPA /= thisStudentAU;
-        System.out.println("GPA for this semester: " + studentGPA);
-        if (studentGPA >= 4.50) {
-            System.out.println("On track of First Class Honor!");
-        } else if (studentGPA >= 4.0) {
-            System.out.println("On track of Second Upper Class Honor!");
-        } else if (studentGPA >= 3.5) {
-            System.out.println("On track of Second Lower Class Honor!");
-        } else if (studentGPA >= 3) {
-            System.out.println("On track of Third Class Honor!");
-        } else {
-            System.out.println("Advice: Study hard");
-        }
-        System.out.println("------------------ End of Transcript -------------------");
-
     }
 
     /**
