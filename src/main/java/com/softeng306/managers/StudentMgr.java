2707e977d86babd5f194e442c8bbb407f0896643
Angstylemon <maxd.gurr@gmail.com>
2020-10-29 20:48:23 +1300

    Removed unused imports from StudentMgr + MainMenuIO

--- src/main/java/com/softeng306/managers/StudentMgr.java
+++ src/main/java/com/softeng306/managers/StudentMgr.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Optional;
-import java.util.Scanner;
 
 /**
  * Manages the student related operations.
