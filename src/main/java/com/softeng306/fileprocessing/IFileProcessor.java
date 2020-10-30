d79b14a304fbf8a7255b5dbfb0584ef995721e7d
Max Gurr <maxd.gurr@gmail.com>
2020-10-31 05:20:38 +1300

    IFileProcessor javadocs

--- src/main/java/com/softeng306/fileprocessing/IFileProcessor.java
+++ src/main/java/com/softeng306/fileprocessing/IFileProcessor.java
@@ -4,32 +4,36 @@ import java.io.*;
 import java.util.List;
 
 /**
- * Handles the processing of loading and writing to files.
+ * Interface that defines responsibilities of loading and writing to files.
  */
 public interface IFileProcessor<T> {
 
     /**
      * Write information from a list into a file.
-     * @param filePath          the path to the file to write into
-     * @param listToWrite the information to write
-     * @throws IOException      if the write could not occur
+     *
+     * @param filePath    The path to the file to write into.
+     * @param listToWrite The information to write.
+     * @throws IOException If the write could not occur.
      */
     void writeToFile(String filePath, List<T> listToWrite) throws IOException;
 
     /**
      * Clears the contents of a file.
-     * @param filePath  the path to the file to clear
+     *
+     * @param filePath The path to the file to clear.
      */
     void clearFileContents(String filePath);
 
     /**
      * Loads a list from a file.
-     * @return  the list that is loaded
+     *
+     * @return The list that is loaded.
      */
     List<T> loadFile();
 
     /**
      * Writes a new object to a file.
+     *
      * @param entry The new object to write.
      */
     void writeNewEntryToFile(T entry);
@@ -37,6 +41,7 @@ public interface IFileProcessor<T> {
     /**
      * Update the contents of a file using a list that contains
      * the updated information
+     *
      * @param updatedList list that contains updated information
      */
     void updateFileContents(List<T> updatedList);
