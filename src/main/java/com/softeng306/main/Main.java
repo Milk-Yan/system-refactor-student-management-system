0a67d6cd60fa6f6cf44594401d6e9f139109f6e7
Max Gurr <maxd.gurr@gmail.com>
2020-10-31 05:18:17 +1300

    Main javadocs

--- src/main/java/com/softeng306/main/Main.java
+++ src/main/java/com/softeng306/main/Main.java
@@ -2,11 +2,15 @@ package com.softeng306.main;
 
 import com.softeng306.io.MainMenuIO;
 
+/**
+ * Startup class - Starts the program.
+ */
 public class Main {
 
     /**
      * The main function of the system.
-     * Command line interface.
+     * Prints the welcome message.
+     * Starts the program.
      *
      * @param args The command line parameters.
      */
