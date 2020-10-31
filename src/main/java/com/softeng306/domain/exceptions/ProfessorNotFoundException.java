af1d043437cbe9d1e7dd0d5e837a96a4e4a9ba00
ArtificialSnow <plin213@aucklanduni.ac.nz>
2020-10-31 14:15:28 +1300

    add class comment to ProfessorNotFoundException

--- src/main/java/com/softeng306/domain/exceptions/ProfessorNotFoundException.java
+++ src/main/java/com/softeng306/domain/exceptions/ProfessorNotFoundException.java
@@ -1,5 +1,9 @@
 package com.softeng306.domain.exceptions;
 
+/**
+ * This exception is thrown when the system is asked to manipulate a specific professor, identified by it's professorId,
+ * and no professor with the given professorId is found in the system.
+ */
 public class ProfessorNotFoundException extends Exception {
 
     public ProfessorNotFoundException(String professorID) {
