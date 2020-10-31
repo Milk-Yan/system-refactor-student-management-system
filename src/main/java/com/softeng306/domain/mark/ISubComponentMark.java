53cbaf4d1f5c7c9fae85ef8a6c08bdc02a4056e0
ArtificialSnow <plin213@aucklanduni.ac.nz>
2020-10-31 14:36:32 +1300

    class comment for ISubComponentMark

--- src/main/java/com/softeng306/domain/mark/ISubComponentMark.java
+++ src/main/java/com/softeng306/domain/mark/ISubComponentMark.java
@@ -3,6 +3,11 @@ package com.softeng306.domain.mark;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.softeng306.domain.course.component.SubComponent;
 
+/**
+ * Interface for a SubComponentMark.
+ * Used to represent a Mark for a SubComponent of a Course.
+ * Provides methods to get SubComponent this mark is for, and to get and set the grade given to this mark.
+ */
 @JsonDeserialize(as = SubComponentMark.class)
 public interface ISubComponentMark {
     SubComponent getSubComponent();
