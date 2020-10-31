6bdbe87d4830cf8a2ba76c2953a01b83ba7f7de4
ArtificialSnow <plin213@aucklanduni.ac.nz>
2020-10-31 14:16:16 +1300

    add class comment to StudentNotFoundException

--- src/main/java/com/softeng306/domain/exceptions/StudentNotFoundException.java
+++ src/main/java/com/softeng306/domain/exceptions/StudentNotFoundException.java
@@ -1,5 +1,9 @@
 package com.softeng306.domain.exceptions;
 
+/**
+ * This exception is thrown when the system is asked to manipulate a specific student, identified by it's studentId,
+ * and no student with the given studentId is found in the system.
+ */
 public class StudentNotFoundException extends Exception {
 
     public StudentNotFoundException(String studentID) {
